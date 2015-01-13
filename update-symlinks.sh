0eb882c80398f87579f3917c6f3f62b7671392d4
Caleb Evans <caleb@calebevans.me>
2015-01-12 20:01:33 -0800

    Add shell script to update symlinks for dotfiles

---
+++ update-symlinks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Create/update home directory symlinks to these files
+
+# Change directory to one containing this file
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Force create symlinks
+ln -sf "$(pwd)/bash_profile" ~/.bash_profile
+ln -sf "$(pwd)/vimrc" ~/.vimrc
+ln -sf "$(pwd)/gitconfig" ~/.gitconfig
