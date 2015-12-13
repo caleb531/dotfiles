d7c4948aa52e0f59497da71da013cab304bd709f
Caleb Evans <caleb@calebevans.me>
2015-12-13 13:28:04 -0800

    Ensure Atom package name regex works for uppercase

--- config/export_config.sh
+++ config/export_config.sh
@@ -3,4 +3,4 @@
 echo "Exporting configuration..."
 
 echo "Updating Atom package list..."
-apm ls --installed --bare | grep -Eo '^[a-z0-9\-]+' > ./atom/packages.txt
+apm ls --installed --bare | grep -Eo '^[A-Za-z0-9\-]+' > ./atom/packages.txt
