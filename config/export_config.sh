156381accad0a297bce2597fffb80d5a9279419d
Caleb Evans <caleb@calebevans.me>
2016-02-05 19:14:35 -0800

    Fix bug where some Atom packages were excluded
    The grep pattern applied when exporting configuration excluded every Atom package whose name contained a digit

--- config/export_config.sh
+++ config/export_config.sh
@@ -3,4 +3,4 @@
 echo "Exporting configuration..."
 
 echo "Updating Atom package list..."
-apm ls --installed --bare | grep -oP '[a-z\-]+' > ./atom/packages.txt
+apm ls --installed --bare | grep -oP '[a-z0-9\-]+(?=@)' > ./atom/packages.txt
