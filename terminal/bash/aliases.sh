7baa6dc0b480418ab480a375f34040d482712ce5
Caleb Evans <caleb@calebevans.me>
2016-03-21 16:24:27 -0700

    Make .bash_profile executable; add octmod alias

--- terminal/bash/aliases.sh
+++ terminal/bash/aliases.sh
@@ -8,6 +8,9 @@ alias sudo='sudo '
 # Colorize directory listings
 alias ls='ls --color=auto'
 
+# Retrieves the octal permissions for a file or directory
+alias octmod='stat -c %a'
+
 # Colorize grep matches (but not for piped output)
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
