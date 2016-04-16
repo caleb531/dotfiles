a2f102357689ac16c01b0f1da58d52ca1789c540
Caleb Evans <caleb@calebevans.me>
2016-04-15 21:39:46 -0700

    Add status messages to ssh-remove-id function

--- terminal/bash/ssh-remove-id.sh
+++ terminal/bash/ssh-remove-id.sh
@@ -6,7 +6,13 @@ __ssh_remove_id() {
 	local escaped_pub_key="$(echo "$pub_key" | sed -e 's/[\/+]/\\&/g')"
 	local pattern="$escaped_pub_key (.*)\$"
 	local auth_keys_file="$HOME/.ssh/authorized_keys"
-	local new_auth_keys="$(cat "$auth_keys_file" | egrep -v "$pattern")"
-	echo "$new_auth_keys" > "$auth_keys_file"
+	local old_auth_keys="$(cat "$auth_keys_file")"
+	local new_auth_keys="$(echo "$old_auth_keys" | egrep -v "$pattern")"
+	if [ "$new_auth_keys" != "$old_auth_keys" ]; then
+		echo "$new_auth_keys" > "$auth_keys_file"
+		echo "1 key removed."
+	else
+		echo "Key not found."
+	fi
 }
 __ssh_remove_id "$*"
