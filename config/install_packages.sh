37990988f2c811ad930d49151d8c7048ba5307e7
Caleb Evans <caleb@calebevans.me>
2016-08-23 09:44:32 -0700

    Add gnu-tar to package installer

--- config/install_packages.sh
+++ config/install_packages.sh
@@ -33,6 +33,7 @@ install_brew_pkg gnu-sed --with-default-names
 # GNU ls is required for colored ls output
 install_brew_pkg coreutils
 install_brew_pkg tree
+install_brew_pkg gnu-tar --with-default-names
 
 install_brew_pkg git
 
