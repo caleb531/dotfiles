04742c4f0a9b06e5d24f90d16c68edd121b70ba1
Caleb Evans <caleb@calebevans.me>
2016-11-24 15:55:51 -0800

    Add pip3 linking logic to create_symlinks.sh
    The logic was originally only present in install_packages.sh, but
    I've since discovered that reinstalling or updating Python 2 via
    Homebrew could potentially override my symlink, making the global
    pip point to pip2. Copy linking logic to create_symlinks so pip3
    can be easily re-linked if the global pip symlink is overriden.

--- setup/create_symlinks.sh
+++ setup/create_symlinks.sh
@@ -25,5 +25,10 @@ ln -snf ~/dotfiles/ssh/ssh_config ~/.ssh/config
 mkdir -p ~/.gnupg
 ln -sf ~/dotfiles/gpg/* ~/.gnupg
 
+# Always use Python 3 pip
+if [ -f /usr/local/bin/pip ]; then
+	ln -sf /usr/local/bin/pip3 /usr/local/bin/pip
+fi
+
 # Disable Bash Sessions feature in OS X El Capitan
 touch ~/.bash_sessions_disable
