cdef1fa1bef2a624e756fa3d85f35ca6cb843bc3
Caleb Evans <caleb@calebevans.me>
2017-02-03 12:20:21 -0800

    Remove speedtest-cli completion removal logic
    Since I removed speedtest-cli from my package installer script, the code
    for removing its unwanted completions is no longer necessary.

--- setup/remove_completions.sh
+++ setup/remove_completions.sh
@@ -7,6 +7,3 @@ rm -f /usr/local/etc/bash_completion.d/brew
 rm -f /usr/local/etc/bash_completion.d/npm
 # Prefer `bundle` over `bundler`
 rm -f /usr/local/bin/bundler
-# Prefer `speedtest` over `speedtest-cli` and `speedtest_cli`
-rm -f /usr/local/bin/speedtest-cli
-rm -f /usr/local/bin/speedtest_cli
