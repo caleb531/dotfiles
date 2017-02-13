437a1bb86da1ead0dc4fe713c3e79f9f1f5ea248
Caleb Evans <caleb@calebevans.me>
2017-02-12 17:37:38 -0800

    Add trailing slash to dir symlinks when autocompleting

--- terminal/bash/config.sh
+++ terminal/bash/config.sh
@@ -10,6 +10,8 @@ shopt -s histappend
 # Navigate command history matching typed input using up/down arrow keys
 bind '"\e[A": history-search-backward' 2> /dev/null
 bind '"\e[B": history-search-forward' 2> /dev/null
+# Automatically add trailing slash to autocompleted directory symlinks
+bind 'set mark-symlinked-directories on' 2> /dev/null
 
 # If shell is Bash 4 or newer
 if [ $BASH_VERSINFO -ge 4 ]; then
