d865088baaf90c272960a7124d176bad87a139f7
Caleb Evans <caleb@calebevans.me>
2017-04-08 15:42:51 -0700

    Rename set_prefs to setup_prefs
    This allows for easier autocompletion of either setup_all or
    setup_prefs.

--- setup/setup_all.sh
+++ setup/setup_all.sh
@@ -3,7 +3,7 @@
 echo "Configuring all..."
 
 ~/dotfiles/setup/create_symlinks.sh
-~/dotfiles/setup/set_prefs.sh
+~/dotfiles/setup/setup_prefs.sh
 ~/dotfiles/setup/install_packages.sh
 ~/dotfiles/setup/install_casks.sh
 ~/dotfiles/setup/remove_completions.sh
