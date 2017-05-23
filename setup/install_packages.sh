36f748f22c269194ae98a9923ca4ecb5fdd19485
Caleb Evans <caleb@calebevans.me>
2017-05-23 13:17:14 -0700

    Do not explicitly link imagemagick@6
    The rmagick Jekyll plugin still functions perfectly even if
    imagemagick@6 isn't linked. Having the package explicitly linked also
    generated an unwelcome status message.

--- setup/install_packages.sh
+++ setup/install_packages.sh
@@ -45,8 +45,6 @@ install_brew_pkg trash
 install_brew_pkg librsvg
 # ImageMagick >=7.0.0 will currently break Ruby gems like rmagick
 install_brew_pkg imagemagick@6
-# imagemagick@6 is keg-only, so it must be linked explicitly
-brew link --force imagemagick@6
 # Pin ImageMagick because upgrading it can break plugins which depend on it
 # (like the rmagick Jekyll plugin I use for my personal website)
 pin_brew_pkg imagemagick
