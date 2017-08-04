4d241afeabd72e0cb91bef08fc7abb5aa884b94b
Caleb Evans <caleb@calebevans.me>
2017-08-03 21:51:53 -0700

    Revert "Add python memory_profiler to package installer"
    This reverts commit a8bed1a068f3d8eaee98db569958bc2352f197a0.

--- setup/install_packages.sh
+++ setup/install_packages.sh
@@ -85,8 +85,6 @@ preload_pip_pkg_list
 
 install_pip_pkg virtualenv
 install_pip_pkg flake8
-install_pip_pkg memory-profiler
-install_pip_pkg psutil
 
 echo "Installing Atom packages..."
 preload_apm_pkg_list
