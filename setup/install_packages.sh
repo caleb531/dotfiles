415783cb5a14c32209467a64afc741cb893907b4
Caleb Evans <caleb@calebevans.me>
2017-10-15 16:43:07 -0700

    Install brew-rmtree alongside other brew packages

--- setup/install_packages.sh
+++ setup/install_packages.sh
@@ -53,6 +53,10 @@ install_brew_pkg pandoc
 install_brew_pkg gnupg@2.0
 install_brew_pkg pinentry-mac
 
+# Install rmtree command for uninstalling packages and their leaf deps
+tap_brew_repo beeftornado/rmtree
+install_brew_pkg brew-rmtree
+
 install_brew_pkg ruby
 pin_brew_pkg ruby
 
@@ -77,10 +81,6 @@ install_npm_pkg gulp-cli
 install_npm_pkg brunch
 install_npm_pkg diff-so-fancy
 install_npm_pkg http-server
-
-# Install rmtree command for uninstalling packages and their leaf deps
-tap_brew_repo beeftornado/rmtree
-install_brew_pkg brew-rmtree
 
 echo "Installing pip packages..."
 preload_pip_pkg_list
