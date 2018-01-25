e43012dd11abc5624f86dde2c1d82cbc74f9a72a
Caleb Evans <caleb@calebevans.me>
2018-01-24 18:53:18 -0800

    Replace node 6.11.5 with 6.12.3
    v6.12.3 is the latest release in the 6.x.x LTS family.

--- setup/install_packages.sh
+++ setup/install_packages.sh
@@ -79,7 +79,7 @@ fi
 source ~/dotfiles/terminal/bash/exports.sh
 
 install_node_version "$DEFAULT_NODE_VERSION"
-install_node_version 6.11.5
+install_node_version 6.12.3
 n "$DEFAULT_NODE_VERSION"
 
 echo "Installing npm packages..."
