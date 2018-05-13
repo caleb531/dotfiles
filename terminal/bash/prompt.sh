caleb531/dotfiles

commit 334dcab93167238af139c26edc144ff626f70343

Fix bug where incomplete virtualenv dir would error

terminal/bash/prompt.sh
@@ -70,7 +70,7 @@ __detect_python_virtualenv() {
 
 	local virtualenv=./"$VIRTUAL_ENV_NAME"
 	# If current directory has a virtualenv that is not itself
-	if [ -d "$virtualenv" -a "$virtualenv" != "$PWD" ]; then
+	if [ -d "$virtualenv"/bin/activate -a "$virtualenv" != "$PWD" ]; then
 		# Activate virtualenv if it is not already active
 		if [ -z "$VIRTUAL_ENV" -o "$VIRTUAL_ENV" != "$virtualenv" ]; then
 			source "$virtualenv"/bin/activate

commit f955d61ed5756464c42e3944d8bfa95bad0b77d4

Fix virtualenv breakage caused by 334dcab

terminal/bash/prompt.sh
@@ -70,7 +70,7 @@ __detect_python_virtualenv() {
 
 	local virtualenv=./"$VIRTUAL_ENV_NAME"
 	# If current directory has a virtualenv that is not itself
-	if [ -d "$virtualenv"/bin/activate -a "$virtualenv" != "$PWD" ]; then
+	if [ -f "$virtualenv"/bin/activate -a "$virtualenv" != "$PWD" ]; then
 		# Activate virtualenv if it is not already active
 		if [ -z "$VIRTUAL_ENV" -o "$VIRTUAL_ENV" != "$virtualenv" ]; then
 			source "$virtualenv"/bin/activate