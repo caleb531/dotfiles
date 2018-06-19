cfe4469c4e27c45ded40d38e7df726df8315cecf
Caleb Evans <caleb@calebevans.me>
2018-06-19 12:25:30 -0700

    Add aliases for gulp

--- terminal/bash/aliases.sh
+++ terminal/bash/aliases.sh
@@ -68,11 +68,13 @@ alias hs='http-server -a localhost -c-1'
 # Serve a directory and open it in web browser
 alias hso='hs -o'
 # Build a Grunt project
-alias gb='grunt build'
+alias gb='[ -f gulpfile.js ] && gulp build || grunt build'
 alias gbf='grunt build:force'
 # Watch a Grunt project, building as needed
-alias gs='grunt serve'
+alias gs='[ -f gulpfile.js ] && gulp serve || grunt serve'
 alias gsf='grunt serve:force'
+# Watch a Gulp project, building as needed
+alias gw='gulp build:watch'
 
 # Dependency installation packages
 
