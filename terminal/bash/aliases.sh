133497606e217f1eeb7b776ceb8fc7d74e10df8a
Caleb Evans <caleb@calebevans.me>
2018-11-03 19:34:34 -0700

    Convert less alias short flags to full flags

--- terminal/bash/aliases.sh
+++ terminal/bash/aliases.sh
@@ -17,7 +17,7 @@ alias tree='tree -C'
 alias rmrf='rm -rf'
 
 # Always enable case-insensitive searches in less (for lowercase queries)
-alias less='less --quit-if-one-screen -iX'
+alias less='less --no-init --quit-if-one-screen --ignore-case'
 
 # Mark a file as executable
 alias x='chmod +x'
