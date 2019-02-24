c7efcd11866d7ada623175cf2f5d1b079c4b9088
Caleb Evans <caleb@calebevans.me>
2019-02-23 19:35:09 -0800

    Add Time Machine setup script

---
+++ setup/setup_tm.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source ~/dotfiles/setup/header.sh
+
+# The path to the cloned Asimov repository
+export ASIMOV_PATH=~/asimov
+
+# Exclude the given directory path from Time Machine
+exclude_dir() {
+	local dir_path="$1"
+	tmutil addexclusion "${dir_path%/}"
+}
+
+# Only back up /Users
+for dir_path in ls -d /*/; do
+	if [[ "$dir_path" != '/Users/' ]]; then
+		echo "$dir_path"
+	fi
+done
+
+# Clone Asimov for excluding development directories
+if [ ! -d "$ASIMOV_PATH" ]; then
+	mkdir -p "$ASIMOV_PATH"
+	git clone https://github.com/caleb531/asimov.git "$ASIMOV_PATH"
+else
+	pushd "$ASIMOV_PATH" > /dev/null || exit
+	git pull > /dev/null
+	popd > /dev/null || exit
+fi
+
+"$ASIMOV_PATH"/install.sh
