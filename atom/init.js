00fc73c5c53dd0b58ee3063cce2602732fb332da
Caleb Evans <caleb@calebevans.me>
2019-03-30 20:52:21 -0700

    Revert "Colorize `super` in addition to `this`"
    This reverts commit 467dad895ea2a8c543889c43ff50319a85b4bdb9.

--- atom/init.js
+++ atom/init.js
@@ -42,9 +42,8 @@ function extendJavaScriptTSGrammar() {
   setImmediate(() => {
     const jsGrammar = atom.grammars.treeSitterGrammarsById['source.js'];
     if (jsGrammar) {
-      // Colorize `this` and `super` in red (like other language keywords)
+      // Colorize `this` in red like other language keywords
       jsGrammar.scopeMap.namedScopeTable.this = {result: 'variable.language.js'};
-      jsGrammar.scopeMap.namedScopeTable.super = {result: 'variable.language.js'};
     }
   });
 }
