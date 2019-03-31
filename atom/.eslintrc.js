04fb3df01e2b4f8f5a84df2e8ad4dc39809bc315
Caleb Evans <caleb@calebevans.me>
2019-03-30 19:59:42 -0700

    Add missing newline to eslintrc (for consistency)

--- atom/.eslintrc.js
+++ atom/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     // enable all ECMAScript 6 features except for modules (this automatically
     // sets the ecmaVersion parser option to 6)
     'es6': true
+
   },
 
   /**
