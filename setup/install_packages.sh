bcec84d5587f4c3cc999238629eeef061190bed7
Caleb Evans <caleb@calebevans.me>
2020-07-21 19:29:24 -0700

    Add icalendar to pip package installer

--- setup/install_packages.sh
+++ setup/install_packages.sh
@@ -100,6 +100,7 @@ preload_pip_pkg_list
 install_pip_pkg virtualenv
 install_pip_pkg flake8
 install_pip_pkg bump-anything
+install_pip_pkg icalendar
 
 echo "Installing Atom packages..."
 preload_apm_pkg_list
