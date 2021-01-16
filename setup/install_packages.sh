2395d0830c334c452ee72bfe99cd1977e54edfab
Caleb Evans <caleb@calebevans.me>
2021-01-16 10:25:40 -0800

    Install Rosetta 2 if Homebrew is not installed

--- setup/install_packages.sh
+++ setup/install_packages.sh
@@ -6,6 +6,8 @@ if ! is_cmd_installed brew; then
 
 	echo "Installing Command Line Tools..."
 	xcode-select --install
+	echo "Installing Rosetta..."
+	sudo softwareupdate --install-rosetta 2> /dev/null
 
 	echo "Installing Homebrew..."
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
