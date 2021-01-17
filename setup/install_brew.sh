0127fda79f973e00a51c25fae84a8b692a2cdd7e
Caleb Evans <caleb@calebevans.me>
2021-01-16 16:13:20 -0800

    Do not export /usr/local/bin when installing Homebrew
    /usr/local/bin is already on the path by default in macOS.

--- setup/install_brew.sh
+++ setup/install_brew.sh
@@ -11,7 +11,6 @@ if ! is_cmd_installed brew; then
 
 	echo "Installing Homebrew..."
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	export PATH=/usr/local/bin:"$PATH"
 	export PATH=/opt/homebrew/bin:"$PATH"
 	export BREW_PREFIX="$(brew --prefix)"
 
