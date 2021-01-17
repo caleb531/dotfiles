9ae01f7c61b3611c3e8a43319689db6abda3ec55
Caleb Evans <caleb@calebevans.me>
2021-01-16 16:07:56 -0800

    Install casks before installing brew packages
    This will ensure that Atom is installed before installing apm packages.

--- setup/setup_all.sh
+++ setup/setup_all.sh
@@ -5,8 +5,8 @@ echo "Configuring all..."
 ~/dotfiles/setup/create_symlinks.sh
 ~/dotfiles/setup/setup_prefs.sh
 ~/dotfiles/setup/install_brew.sh
-~/dotfiles/setup/install_packages.sh
 ~/dotfiles/setup/install_casks.sh
+~/dotfiles/setup/install_packages.sh
 ~/dotfiles/setup/install_mas_apps.sh
 ~/dotfiles/setup/install_fonts.sh
 ~/dotfiles/setup/set_default_apps.sh
