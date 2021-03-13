0d5e9d90d3311a4f3693be03c7ef9091c3c91389
Caleb Evans <caleb@calebevans.me>
2021-03-12 16:12:31 -0800

    Replace Node 12.14.1 with Node 12.16.3

--- setup/install_packages.sh
+++ setup/install_packages.sh
@@ -80,8 +80,8 @@ source ~/dotfiles/terminal/bash/load_nvm.sh
 nvm install 6.15.1
 nvm install 8.9.4
 nvm install 10.16.3
-nvm install 12.14.1
-nvm alias default 12.14.1
+nvm install 12.16.3
+nvm alias default 12.16.3
 
 echo "Installing npm packages..."
 preload_npm_pkg_list
