6fcb649956ea7aa98a2477c0892f4431df5b6cd0
Caleb Evans <caleb@calebevans.me>
2021-07-06 10:11:49 -0700

    Move node installer to dedicated setup script

---
+++ setup/install_node.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+source ~/dotfiles/setup/header.sh
+# Load in environment variables for brew, nvm, node, etc.
+source ~/dotfiles/terminal/bash/exports.sh
+
+echo "Installing node..."
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+mkdir -p ~/.nvm
+source ~/dotfiles/terminal/bash/load_nvm.sh
+nvm install 6.15.1
+nvm install 8.9.4
+nvm install 10.16.3
+nvm install 12.16.3
+nvm alias default 12.16.3
+
+echo "Installing npm packages..."
+preload_npm_pkg_list
+
+install_npm_pkg gulp-cli
+install_npm_pkg http-server
+install_npm_pkg grunt-cli
+install_npm_pkg vsce
--- setup/install_packages.sh
+++ setup/install_packages.sh
@@ -72,24 +72,7 @@ preload_gem_list
 install_gem bundler
 install_gem jekyll
 
-echo "Installing node..."
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-mkdir -p ~/.nvm
-source ~/dotfiles/terminal/bash/load_nvm.sh
-nvm install 6.15.1
-nvm install 8.9.4
-nvm install 10.16.3
-nvm install 12.16.3
-nvm alias default 12.16.3
-
-echo "Installing npm packages..."
-preload_npm_pkg_list
-
-install_npm_pkg gulp-cli
-install_npm_pkg http-server
-install_npm_pkg grunt-cli
-install_npm_pkg vsce
+~/dotfiles/setup/install_node.sh
 
 echo "Installing pip packages..."
 preload_pip_pkg_list
