b36e9aa4864976272f3075ad0d340870d1367fd7
Caleb Evans <caleb@calebevans.me>
2021-07-05 20:07:49 -0700

    Add nvm to node remover script

--- setup/remove_node.sh
+++ setup/remove_node.sh
@@ -3,6 +3,7 @@
 if type brew &> /dev/null; then
 	brew uninstall node
 	brew uninstall n
+	brew uninstall nvm
 fi
 
 sudo rm -rfv \
@@ -14,13 +15,16 @@ sudo rm -rfv \
 	~/.node-gyp \
 	/opt/local/bin/node \
 	/opt/local/include/node \
+	/usr/local/opt/nvm \
 	/opt/local/lib/node_modules\
 	/usr/local/lib/node_modules \
 	/usr/local/lib/npm-packages \
 	/opt/homebrew/lib/npm-packages \
+	/opt/homebrew/opt/nvm \
 	/usr/local/include/node \
 	/usr/local/n \
-	~/n
+	~/n \
+	~/.nvm \
 
 if type brew &> /dev/null; then
 	brew prune
