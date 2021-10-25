8e62b11a7f6541c00d738e1659e4be427321d131
Caleb Evans <caleb@calebevans.me>
2021-10-24 20:28:37 -0700

    Do not auto-commit after exporting VS Code extension
    This is because I may be trying out an extension and do not necessarily
    want to commit it to my dotfiles straight away. However, I would still
    certainly want to pull on my dotfiles repository when running the
    install script.

--- setup/export_vscode_extensions.sh
+++ setup/export_vscode_extensions.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
 code --list-extensions | sort -f > ~/dotfiles/vscode/extensions.txt
-pushd ~/dotfiles &> /dev/null || exit
-git reset &> /dev/null
-git add ~/dotfiles/vscode/extensions.txt
-git commit -m 'Update VS Code extensions list' 2> /dev/null
-git push 2> /dev/null
-popd &> /dev/null
