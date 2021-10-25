94e9c58b71dd98146d8438312d8550c55a54f59d
Caleb Evans <caleb@calebevans.me>
2021-10-25 10:14:06 -0700

    Add return types to VS Code init script

--- vscode/init.ts
+++ vscode/init.ts
@@ -6,17 +6,17 @@ import * as path from 'path';
 import { exec } from 'child_process';
 
 // Export full list of installed extensions
-function exportExtensions() {
+function exportExtensions(): void {
   exec(path.join('~', 'dotfiles', 'setup', 'export_vscode_extensions.sh'));
 }
 
 
 // Install synced extensions not already installed on this machine
-function installLatestExtensions() {
+function installLatestExtensions(): void {
   exec(path.join('~', 'dotfiles', 'setup', 'install_vscode_extensions.sh'));
 }
 
-export function init(context: ExtensionContext) {
+export function init(context: ExtensionContext): void {
   // Detect when an extension is installed, uninstalled, enabled, or disabled
   extensions.onDidChange(() => exportExtensions());
   installLatestExtensions();
