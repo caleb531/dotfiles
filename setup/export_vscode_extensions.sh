82cad965e53d690675b7b1bab9401d9376ca2fd1
Caleb Evans <caleb@calebevans.me>
2021-10-24 17:52:39 -0700

    Add better error-handling to extension export script

--- setup/export_vscode_extensions.sh
+++ setup/export_vscode_extensions.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 code --list-extensions | sort -f > ~/dotfiles/vscode/extensions.txt
+git reset &> /dev/null
 git add ~/dotfiles/vscode/extensions.txt
-git commit -m 'Updated VS Code extensions list'
-git push
+git commit -m 'Updated VS Code extensions list' 2> /dev/null
+git push 2> /dev/null
