65d2d8bb6734c22270de847f6ba6f838879de931
Caleb Evans <caleb@calebevans.me>
2021-10-25 21:28:21 -0700

    Undo broken extension sync functionality

--- vscode/init.ts
+++ vscode/init.ts
@@ -1,23 +1,6 @@
-// @ts-ignore: ignore vscode module import
-import { extensions, ExtensionContext } from 'vscode';
-// @ts-ignore: ignore Path module import
-import * as path from 'path';
-// @ts-ignore: ignore child_process module import
-import { exec } from 'child_process';
-
-// Export full list of installed extensions
-function exportExtensions(): void {
-  exec(path.join('~', 'dotfiles', 'setup', 'export_vscode_extensions.sh'));
-}
-
-
-// Install synced extensions not already installed on this machine
-function installLatestExtensions(): void {
-  exec(path.join('~', 'dotfiles', 'setup', 'install_vscode_extensions.sh'));
-}
+// @ts-ignore: ignore vscode import
+import { ExtensionContext } from 'vscode';
 
 export function init(context: ExtensionContext): void {
-  // Detect when an extension is installed, uninstalled, enabled, or disabled
-  extensions.onDidChange(() => exportExtensions());
-  // installLatestExtensions();
+  // Do nothing for now
 }
