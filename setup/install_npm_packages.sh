c35adc6fd525929ccf79c855243a4074a126c8c1
Caleb Evans <caleb@calebevans.me>
2022-08-10 13:25:40 -0700

    Fix bug where npm package installer could not find npm

--- setup/install_npm_packages.sh
+++ setup/install_npm_packages.sh
@@ -3,6 +3,8 @@
 source ~/dotfiles/setup/header.sh
 # Load in environment variables for brew, nvm, node, etc.
 source ~/dotfiles/terminal/bash/exports.sh
+# Load nvm so that the node and npm commands are available
+source ~/dotfiles/terminal/bash/load_nvm.sh
 
 echo "Installing npm packages..."
 preload_npm_pkg_list
