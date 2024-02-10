ec1e34dcdd339d2fd2a5708153451833b97cb1a5
Caleb Evans <caleb@calebevans.me>
2024-02-10 14:00:51 -0800

    Auto-format Python files with black

--- setup/setup_spotlight.py
+++ setup/setup_spotlight.py
@@ -4,44 +4,45 @@ import argparse
 import contextlib
 import functools
 import glob
+import hashlib
 import os
 import os.path
-import hashlib
 import shutil
 import subprocess
 import sys
 
-
-DEFAULT_EXCLUSIONS_LIST_PATH = os.path.expanduser(os.path.join(
-    '~', 'dotfiles', 'private', 'setup', 'spotlight-exclusions.txt'))
-EXCLUSIONS_SYMLINK_DIR = os.path.expanduser(os.path.join(
-    '~', 'spotlight-exclusions'))
+DEFAULT_EXCLUSIONS_LIST_PATH = os.path.expanduser(
+    os.path.join("~", "dotfiles", "private", "setup", "spotlight-exclusions.txt")
+)
+EXCLUSIONS_SYMLINK_DIR = os.path.expanduser(os.path.join("~", "spotlight-exclusions"))
 
 
 @functools.lru_cache()
 def get_indexed_paths(name):
-    return subprocess.check_output([
-        'mdfind',
-        '-name',
-        name
-    ]).decode('utf-8').strip().split('\n')
+    return (
+        subprocess.check_output(["mdfind", "-name", name])
+        .decode("utf-8")
+        .strip()
+        .split("\n")
+    )
 
 
 def is_indexed(exclusion_path):
     basename = os.path.basename(exclusion_path)
-    return (exclusion_path in get_indexed_paths(basename))
+    return exclusion_path in get_indexed_paths(basename)
 
 
 def get_symlink_checksum(exclusion_path):
-    return hashlib.md5(exclusion_path.encode('utf-8')).hexdigest()
+    return hashlib.md5(exclusion_path.encode("utf-8")).hexdigest()
 
 
 def get_symlink_filename(exclusion_path):
     return os.path.join(
         EXCLUSIONS_SYMLINK_DIR,
-        '{}-{}'.format(
-            os.path.basename(exclusion_path),
-            get_symlink_checksum(exclusion_path)))
+        "{}-{}".format(
+            os.path.basename(exclusion_path), get_symlink_checksum(exclusion_path)
+        ),
+    )
 
 
 def process_exclusion(exclusion_glob):
@@ -52,14 +53,15 @@ def process_exclusion(exclusion_glob):
         did_run = True
         with contextlib.suppress(OSError):
             if os.path.isdir(exclusion_path) and is_indexed(exclusion_path):
-                os.symlink(
-                    exclusion_path,
-                    get_symlink_filename(exclusion_path))
-                print(exclusion_path.replace(os.path.expanduser('~'), '~'))
+                os.symlink(exclusion_path, get_symlink_filename(exclusion_path))
+                print(exclusion_path.replace(os.path.expanduser("~"), "~"))
     if not did_run:
-        print('No matches found: {}'.format(
-            exclusion_glob.replace(os.path.expanduser('~'), '~')),
-            file=sys.stderr)
+        print(
+            "No matches found: {}".format(
+                exclusion_glob.replace(os.path.expanduser("~"), "~")
+            ),
+            file=sys.stderr,
+        )
 
 
 def recreate_symlink_directory():
@@ -73,16 +75,15 @@ def process_exclusions(exclusion_globs):
     recreate_symlink_directory()
     for exclusion_glob in exclusion_globs:
         if exclusion_glob.strip():
-            process_exclusion(exclusion_glob.strip().rstrip('/'))
+            process_exclusion(exclusion_glob.strip().rstrip("/"))
 
 
 def parse_cli_args():
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'exclusions_list_path',
-        nargs='?',
-        default=DEFAULT_EXCLUSIONS_LIST_PATH)
+        "exclusions_list_path", nargs="?", default=DEFAULT_EXCLUSIONS_LIST_PATH
+    )
     return parser.parse_args()
 
 
@@ -90,11 +91,11 @@ def main():
 
     cli_args = parse_cli_args()
 
-    with open(cli_args.exclusions_list_path, 'r') as exclusions_list_file:
+    with open(cli_args.exclusions_list_path, "r") as exclusions_list_file:
         process_exclusions(exclusions_list_file.readlines())
     if os.listdir(EXCLUSIONS_SYMLINK_DIR):
-        subprocess.call(['open', EXCLUSIONS_SYMLINK_DIR])
+        subprocess.call(["open", EXCLUSIONS_SYMLINK_DIR])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
