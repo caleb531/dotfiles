caleb531/dotfiles

commit eb5183f07e15c16c7e8ba287045558cde9c3b801

Enforce pnpm v8
For compatibility reasons.

setup/helper_functions.sh
@@ -41,7 +41,12 @@ is_node_version_installed() {
 }
 
 is_npm_pkg_installed() {
-	echo "$NPM_PKG_LIST" | grep --quiet " $1@"
+	if echo "$1" | grep --extended-regexp --quiet ".@."; then
+		# If package argument contains version specifier, account for that
+		echo "$NPM_PKG_LIST" | grep --quiet " $1"
+	else
+		echo "$NPM_PKG_LIST" | grep --quiet " $1@"
+	fi
 }
 
 is_cask_installed() {

setup/install_npm_packages.sh
@@ -13,7 +13,7 @@ fi
 echo "Installing npm packages (node $(node -v))..."
 preload_npm_pkg_list
 
-install_npm_pkg pnpm
+install_npm_pkg pnpm@^8.0.0
 install_npm_pkg gulp-cli
 install_npm_pkg eslint
 install_npm_pkg http-server

commit 6da5d080178f64affd50af7747553e1e7dca950b

Ignore version specifier when checking if npm package is installed
This fixes some broken logic from eb5183f.

setup/helper_functions.sh
@@ -43,7 +43,8 @@ is_node_version_installed() {
 is_npm_pkg_installed() {
 	if echo "$1" | grep --extended-regexp --quiet ".@."; then
 		# If package argument contains version specifier, account for that
-		echo "$NPM_PKG_LIST" | grep --quiet " $1"
+		echo "$NPM_PKG_LIST" \
+			| grep --quiet " $(echo "$1" | grep -Po '.+(?=@)')"
 	else
 		echo "$NPM_PKG_LIST" | grep --quiet " $1@"
 	fi