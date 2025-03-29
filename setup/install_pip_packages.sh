730a18910059b2c97c38e46d457646b02cb59aa7
Caleb Evans <caleb@calebevans.me>
2025-03-29 13:39:05 -0700

    Remove aider
    Superseded by GitHub Copilot.

--- setup/install_pip_packages.sh
+++ setup/install_pip_packages.sh
@@ -9,7 +9,4 @@ preload_pip_pkg_list
 install_pip_pkg virtualenv
 install_pip_pkg flake8
 install_pip_pkg bump-anything
-# Aider does not support Python 3.13 yet (source:
-# <https://github.com/Aider-AI/aider/issues/2391#issuecomment-2525674695>)
-install_pip_pkg aider-chat --python python3.12
 install_pip_pkg imessage-conversation-analyzer
