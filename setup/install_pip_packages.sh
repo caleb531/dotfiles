a43f9c0a7bb22e0afe3e4d7d286ecf12eaf01bb0
Caleb Evans <caleb@calebevans.me>
2025-06-23 20:35:26 -0700

    Add uv to pip package installer
    Replaces the virtualenv package.

--- setup/install_pip_packages.sh
+++ setup/install_pip_packages.sh
@@ -6,7 +6,7 @@ source ~/dotfiles/terminal/bash/exports.sh
 echo "Installing pip packages..."
 preload_pip_pkg_list
 
-install_pip_pkg virtualenv
+install_pip_pkg uv
 install_pip_pkg flake8
 install_pip_pkg bump-anything
 install_pip_pkg imessage-conversation-analyzer
