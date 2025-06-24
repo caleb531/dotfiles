76325c19d8fcad51d050914c8c824d507f492288
Caleb Evans <caleb@calebevans.me>
2025-06-23 20:37:54 -0700

    Add ruff to global python packages

--- setup/install_pip_packages.sh
+++ setup/install_pip_packages.sh
@@ -7,6 +7,6 @@ echo "Installing pip packages..."
 preload_pip_pkg_list
 
 install_pip_pkg uv
-install_pip_pkg flake8
+install_pip_pkg ruff
 install_pip_pkg bump-anything
 install_pip_pkg imessage-conversation-analyzer
